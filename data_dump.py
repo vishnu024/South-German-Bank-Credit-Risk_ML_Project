85e5714dca39ae6410f3277b8adaaafccd4192d7
vishnu024 <vishnu1997ksr@gmail.com>
2023-01-10 22:11:54 +0530

    Added Data Ingestion component and this is the first component release

---
+++ data_dump.py
@@ -0,0 +1,32 @@
+import pymongo
+import pandas as pd
+import json
+
+from credit.config import mongo_client
+
+
+DATABASE_NAME= "german_bank"
+COLLECTION_NAME= "credit_data"
+DATA_FILE_PATH= "/config/workspace/processed_South_German_Bank_Credit_Risk_dataset.csv"
+
+
+if __name__=="__main__":
+    df= pd.read_csv(DATA_FILE_PATH)
+    print(f"No. of rows and columns are:",{df.shape})
+
+    # Convert Data frame to JSON so that we can store the data in MongoDB
+
+    df.reset_index(inplace=True, drop=True)
+
+    json_record= list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    #Inserting the records to Mongo DB
+
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import pymongo
+from credit.exception import CreditException
+from credit.logger import logging
+from credit.pipeline.training_pipeline import start_training_pipeline
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+file_path="/config/workspace/processed_South_German_Bank_Credit_Risk_dataset.csv"
+print(__name__)
+if __name__=="__main__":
+     try:
 
-# Database Name
-dataBase = client["neurolabDB"]
+          
 
-# Collection  Name
-collection = dataBase['Products']
+          #data ingestion
+          start_training_pipeline()
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+          '''
+          start_training_pipeline()
+          output_file = start_batch_prediction(input_file_path=file_path)
+          print(output_file)
+          '''
+     except Exception as e:
+          print(e)
