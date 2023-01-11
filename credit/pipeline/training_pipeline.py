92a1ad2de759de89918dad7432d224b8ad1fccbb
vishnu024 <vishnu1997ksr@gmail.com>
2023-01-11 11:00:52 +0530

    Added Data Validation Component

--- credit/pipeline/training_pipeline.py
+++ credit/pipeline/training_pipeline.py
@@ -4,8 +4,8 @@ from credit.utils import get_collection_as_dataframe
 import sys,os
 from credit.entity import config_entity
 from credit.components.data_ingestion import DataIngestion
-'''
 from credit.components.data_validation import DataValidation
+'''
 from credit.components.data_transformation import DataTransformation
 from credit.components.model_trainer import ModelTrainer
 from credit.components.model_evaluation import ModelEvaluation
@@ -21,13 +21,14 @@ def start_training_pipeline():
         print(data_ingestion_config.to_dict())
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-        '''
+        
         #data validation
         data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
         data_validation = DataValidation(data_validation_config=data_validation_config,
                         data_ingestion_artifact=data_ingestion_artifact)
 
         data_validation_artifact = data_validation.initiate_data_validation()
+        '''
 
         #data transformation
         data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
@@ -59,4 +60,4 @@ def start_training_pipeline():
 
         '''
     except Exception as e:
-        raise SensorException(e, sys)
+        raise CreditException(e, sys)
