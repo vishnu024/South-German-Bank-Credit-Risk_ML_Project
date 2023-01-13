d2970413f195d0ebae165bc6e6a1fefdb8cf81e0
vishnu024 <vishnu1997ksr@gmail.com>
2023-01-13 22:50:03 +0530

    Updated code for app.py

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 from credit.predictor import ModelResolver
 from credit.entity import config_entity,artifact_entity
 from credit.exception import CreditException
+from credit.logger import logging
+from credit.utils import load_object
+from sklearn.metrics import f1_score
+import pandas  as pd
+import sys,os
+from credit.config import TARGET_COLUMN
 
-
-st.title('Classifying Iris Flowers')
-st.markdown('Toy model to play to classify iris flowers into \
+model = load_object(file_path=model_path)
+st.title('Classifying Credit')
+st.markdown('model to classify Credit into \
      (setosa, versicolor, virginica) based on their sepal/petal \
     and length/width.')
 
-st.header("Plant Features")
+st.header("Credit data")
 col1, col2 = st.columns(2)
 
 with col1:
     st.text("Sepal characteristics")
-    sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-    sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+    status = st.slider('status', 1.0, 8.0, 0.5)
+    duration = st.slider('duration', 2.0, 4.4, 0.5)
+    credit_history = st.slider('credit_history', 2.0, 4.4, 0.5)
+    purpose = st.slider('purpose', 2.0, 4.4, 0.5)
+    amount = st.slider('amount', 2.0, 4.4, 0.5)
+    savings = st.slider('savings', 2.0, 4.4, 0.5)
+    employment_duration = st.slider('employment_duration', 2.0, 4.4, 0.5)
+    installment_rate = st.slider('installment_rate', 2.0, 4.4, 0.5)
+    personal_status_sex = st.slider('personal_status_sex', 2.0, 4.4, 0.5)
+    propertyl = st.slider('property', 2.0, 4.4, 0.5)
+    age = st.slider('age', 2.0, 4.4, 0.5)
+    number_credits = st.slider('number_credits', 2.0, 4.4, 0.5)
+    job = st.slider('job', 2.0, 4.4, 0.5)
 
 with col2:
     st.text("Pepal characteristics")
-    petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-    petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+    people_liable = st.slider('people_liable', 1.0, 7.0, 0.5)
+    telephone = st.slider('telephone', 0.1, 2.5, 0.5)
 
 st.text('')
 if st.button("Predict type of Iris"):
     result = model.predict(
-        np.array([[sepal_l, sepal_w, petal_l, petal_w]]))
+        np.array([[status, duration, credit_history, purpose, amount, savings, employment_duration, installment_rate, personal_status_sex, propertyl, age, number_credits, job, people_liable, telephone, credit_risk]]))
     st.text(result[0])
 
 
