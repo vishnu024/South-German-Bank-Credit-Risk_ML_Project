2deaeb11464b57afc41752399a65fcd7af4c0279
vishnu024 <vishnu1997ksr@gmail.com>
2023-01-14 17:02:59 +0530

    Update code for app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ if st.button("Predict the Credibility"):
     data = np.array([[status, duration, credit_history, purpose, amount, savings, employment_duration, installment_rate, personal_status_sex, propertyl, age, number_credits, job, people_liable, telephone]])
     data = pd.DataFrame(data, columns=['status', 'duration', 'credit_history', 'purpose', 'amount', 'savings', 'employment_duration', 'installment_rate', 'personal_status_sex', 'propertyl', 'age', 'number_credits', 'job', 'people_liable', 'telephone'])
     transformed = transformer.transform(data)
-    result= model.predict(np.array(transformed))
+result= model.predict(np.array(transformed))
     #result = model.predict(
                     #np.array([[status, duration, credit_history, purpose, amount, savings, employment_duration, installment_rate, personal_status_sex, propertyl, age, number_credits, job, people_liable, telephone]]))
   
