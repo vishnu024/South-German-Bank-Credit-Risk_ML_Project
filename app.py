d106a83cda523f054bbc27c9ccad2f3094a5f76f
vishnu024 <vishnu1997ksr@gmail.com>
2023-01-14 17:06:10 +0530

    Update code for app.py

--- app.py
+++ app.py
@@ -156,15 +156,15 @@ with col2:
 
 st.text('')
 if st.button("Predict the Credibility"):
+
     data = np.array([[status, duration, credit_history, purpose, amount, savings, employment_duration, installment_rate, personal_status_sex, propertyl, age, number_credits, job, people_liable, telephone]])
-    data = pd.DataFrame(data, columns=['status', 'duration', 'credit_history', 'purpose', 'amount', 'savings', 'employment_duration', 'installment_rate', 'personal_status_sex', 'propertyl', 'age', 'number_credits', 'job', 'people_liable', 'telephone'])
-transformed = transformer.transform(data)
-result= model.predict(np.array(transformed))
+    datat = pd.DataFrame(data, columns=['status', 'duration', 'credit_history', 'purpose', 'amount', 'savings', 'employment_duration', 'installment_rate', 'personal_status_sex', 'propertyl', 'age', 'number_credits', 'job', 'people_liable', 'telephone'])
+    transformed = transformer.transform(datat)
+    result= model.predict(np.array(transformed))
     #result = model.predict(
                     #np.array([[status, duration, credit_history, purpose, amount, savings, employment_duration, installment_rate, personal_status_sex, propertyl, age, number_credits, job, people_liable, telephone]]))
   
- 
-st.text(result[0])
+    st.text(result[0])
 
 
 st.text('')
